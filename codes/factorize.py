e976fed41345917e83b301309cb652f4cb7849e6
Kuzmacosmos <54091542+Kuzmacosmos@users.noreply.github.com>
2021-06-24 00:53:55 +0800

    Update factorize.py

--- codes/factorize.py
+++ codes/factorize.py
@@ -25,12 +25,14 @@ def factor(num: int) -> List[int]:
 # I plan to write a function returning a dictionary later times.
 
 def find_prime(n_i: int, n_f: int) -> List[int]:
-    """Return a list containing prime integers ranging from two integers: n_i to n_f, inclusive.
+    """Return a list containing prime integers ranging from two 
+    integers: n_i to n_f, inclusive.
     
     >>> find_prime(2, 5)
     [2, 3, 5]
     >>> find_prime(10, 100)
-    [11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    [11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,\
+    61, 67, 71, 73, 79, 83, 89, 97]
     """
     
     result = []
